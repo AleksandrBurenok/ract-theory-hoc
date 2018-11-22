a1888ee3d25068af0bf9a31715d577d8493f4305
AleksandrBurenok <oleksandrburenok@gmail.com>
2018-11-22 13:10:52 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const starWarsChars = [
+    {name: 'Дарт Вейдер', side: 'dark'},
+    {name: 'Люк Скайуокер', side: 'light'},
+    {name: 'Палпатин', side: 'dark'},
+    {name: 'Оби-Ван Кеноби', side: 'light'}
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = ({list}) => (
+    <ul>
+        {list.map((char, index) => {
+            return (
+                <li key={char.name + index}>
+                    <strong>{char.name}</strong> - &nbsp;
+                    <i>{char.side}</i>
+                </li>
+            )
+        })}
+    </ul>
+);
+
+const withFilteredProps = Component => ({list, side}) => {
+    const filterList = list.filter(char => char.side === side);
+    return <Component list={filterList}/>
+};
+
+const FilteredList = withFilteredProps(App);
+
+ReactDOM.render(<FilteredList list={starWarsChars} side='light'/>, document.getElementById('root'));
